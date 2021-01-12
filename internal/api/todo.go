ad1041341b9efb6da85532fa2da4216bf9bf20c7
Trong Bui <trong.buiquoc@gmail.com>
2021-01-12 17:07:18 +0700

    Add endpoints and handlers signature for todo api

--- internal/api/todo.go
+++ internal/api/todo.go
@@ -1,1 +1,23 @@
 package api
+
+import "net/http"
+
+func (s *Server) getTodoList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) addTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) updateTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) completeTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) deleteTodo(w http.ResponseWriter, r *http.Request) {
+
+}
